33816e9160ea7405104d7426e41e34188576f1ff
Nodirbek-Abdulaxadov <nbkabdulaxadov@gmail.com>
2022-03-30 16:54:30 +0500

    qsa

--- BackEnd/E-Learning/E-Learning.Learning/Controllers/HomeController.cs
+++ BackEnd/E-Learning/E-Learning.Learning/Controllers/HomeController.cs
@@ -2,23 +2,17 @@
 using E_Learning.Learning.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace E_Learning.Learning.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
         private readonly IFileInterface _fileInterface;
 
         public HomeController(ILogger<HomeController> logger,
                                 IFileInterface fileInterface)
         {
-            _logger = logger;
             _fileInterface = fileInterface;
         }
         [HttpGet]
@@ -26,6 +20,7 @@ namespace E_Learning.Learning.Controllers
         {
             return View();
         }
+
         [HttpPost]
         public IActionResult Index(FileUpload fileUpload)
         {
@@ -33,15 +28,9 @@ namespace E_Learning.Learning.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult Courses()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
